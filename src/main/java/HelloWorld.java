63aaf209992c2189369ffeec36942227dd38def2
wielbi <wg.ratownik@gmail.com>
2020-11-24 18:25:09 +0100

    jest źle

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Grzegorz");
+        System.out.println("39");
+        System.out.println("W");
+    }
+
+
+}
---
+++ src/main/java/MojeDane.java
@@ -0,0 +1,8 @@
+public class MojeDane {
+        public static void main(String[] args) {
+            String myName = "Maciej";
+            int myAge = 22;
+            double height = 1.78;
+            char carClass = 'A';
+            System.out.println (myName);
+        }}
