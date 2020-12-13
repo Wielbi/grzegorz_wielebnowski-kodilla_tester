6209a25356ab52e5912fec35cf84dd654898e22d
wielbi <wg.ratownik@gmail.com>
2020-12-13 19:37:29 +0100

    ChekPrice

--- kodilla-intro/src/main/java/Notebook.java
+++ kodilla-intro/src/main/java/Notebook.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class Notebook {
+public class Notebook {
+    String weight;
+    int price;
+
+    public Notebook(String weight, int price) {
+        this.weight = weight;
+        this.price = price;
+    }
+
+    public void checkPrice() {
+
+        if (this.price < 600 ) {
+            System.out.println("This notebook is very cheap.");
+
+         if (this.price <= 600-1000){
+             System.out.println("The price is good.");
+
+         if (this.price > 1000){
+             System.out.println("This notebook is expensive.");
+
+         }
+         }
+        } else {
+            System.out.println("This notebook is quite expensive.");
+        }
+    }
 }
+
+
+
+
+
