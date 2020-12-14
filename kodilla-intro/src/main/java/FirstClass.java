2ab495d3037978a19b98f463ae1d2b0ca8535c41
wielbi <wg.ratownik@gmail.com>
2020-12-14 17:08:42 +0100

    Zadanie Komputer rok, waga

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,17 +1,17 @@
 public class  FirstClass {
     public static void main(String[] args) {
 
-        Notebook notebook = new Notebook( 100 ,2000);
-        System.out.println( notebook.year + " " + notebook.price );
+        Notebook notebook = new Notebook( 100 ,2000,  150 );
+        System.out.println( notebook.year + " " + notebook.price + " " + notebook.weight );
         notebook.checkPrice();
 
-        Notebook heavyNotebook = new Notebook ( 1500 , 2020);
-        System.out.println (heavyNotebook.year + " " + heavyNotebook.price);
+        Notebook heavyNotebook = new Notebook ( 100 , 2000, 200 );
+        System.out.println (heavyNotebook.year + " " + heavyNotebook.price + " "+ heavyNotebook.weight);
         heavyNotebook.checkPrice();
 
 
-        Notebook oldNotebook = new Notebook (2000, 2021);
-        System.out.println (oldNotebook.year + " " + oldNotebook.price);
+        Notebook oldNotebook = new Notebook (200, 2000,100  );
+        System.out.println (oldNotebook.year + " " + oldNotebook.price + " " +notebook.weight  );
         oldNotebook.checkPrice();
 
 
--- kodilla-intro/src/main/java/Notebook.java
+++ kodilla-intro/src/main/java/Notebook.java
@@ -1,34 +1,44 @@
 public class Notebook {
     int price;
     int year;
+    int weight;
 
 
-
-    public Notebook( int price, int year) {
+    public Notebook(int price, int year, int weight) {
         this.price = price;
         this.year = year;
+        this.weight = weight;
     }
+
     public void checkPrice() {
 
         if (this.price < 130) {
-            System.out.println("The computer is old.");}
-        {
-            if (this.price > 100) {
-                System.out.println("The computer is expensive .");
-                {
-
-            }
-        } else {
-                System.out.println(" The computer is priced right for its year of manufacture ");
-            }
-
+            System.out.println("The computer is old.");
         }
 
+        if (this.year == 2000) {
+            System.out.println("The computer is too old.");
+        }
+        if (150 > this.weight) {
+            System.out.println("The computer is quite light");
+        }
+        if (this.price > 100) {
+            System.out.println("The computer is expensive .");
+            {
+
+            }
+            if (this.price < 200) {
+                System.out.println(" The computer is priced right for its year of manufacture ");
+
+            }
+        }else{
+            System.out.println("Ten komputer jest zbyt stary i cieżki");
+
+
+        }
     }
-
-
-
 }
+
 
 
 
