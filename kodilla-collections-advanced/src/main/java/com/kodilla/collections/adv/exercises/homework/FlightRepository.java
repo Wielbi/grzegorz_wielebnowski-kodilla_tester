7d1198b19c816a43b352bb7be2a1cbcbf3f65087
wielbi <wg.ratownik@gmail.com>
2021-03-24 18:50:16 +0100

    Zadanie dom1

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/exercises/homework/FlightRepository.java
@@ -0,0 +1,15 @@
+package com.kodilla.collections.adv.exercises.homework;
+
+import java.util.*;
+
+public class FlightRepository {
+
+
+
+
+public static List<Flight> getFlightsTable(){
+
+    Flight
+
+}
+}
