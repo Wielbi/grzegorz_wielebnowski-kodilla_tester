239837a1fc791eebe71791afde96c158699ecc7a
wielbi <wg.ratownik@gmail.com>
2021-04-12 12:50:17 +0200

    zad 7.2

--- kodilla-advanced-tests/src/test/java/parametrized_tests/homework/GamblingMachineTestSuite.java
+++ kodilla-advanced-tests/src/test/java/parametrized_tests/homework/GamblingMachineTestSuite.java
@@ -1,5 +1,6 @@
 package parametrized_tests.homework;
 
+import org.junit.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
@@ -7,8 +8,13 @@ import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
 public class GamblingMachineTestSuite {
+
+    @Test(expected = InvalidNumbersException.class)
+    public void testThrowException() throws InvalidNumbersException {
+        GamblingMachine gamblingMachine = new GamblingMachine();
+        gamblingMachine.howManyWins(Set.of(1,2,3,4,5,6,7,8,9));
+    }
 
     @ParameterizedTest
     @CsvFileSource(resources = "/data.csv")
